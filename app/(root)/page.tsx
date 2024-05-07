4623c6708b2884945eec035ecded21e1ad66a00d
Gabriel Zouad <z20gab@gmail.com>
2024-05-08 00:11:33 +0200

    Implement complete layout and sidebars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -28,7 +28,11 @@ const Home = () => {
         </header>
         RECENT TRANSACTIONS
       </div>
-      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 200.32 }, { currentBalance: 232.69 }]}
+      />
     </section>
   );
 };
